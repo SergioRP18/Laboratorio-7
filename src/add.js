d70f401d6dc838dd42eb02b290e5bd77db70f0af
SergioRP18 <sergio.restrepo@u.icesi.edu.co>
2024-05-19 00:19:15 -0500

    termina implementacion de laboratorio 15 en el laboratorio 7

---
+++ src/add.js
@@ -0,0 +1,37 @@
+const localStorage = window.localStorage
+
+init();
+
+function init(){
+    if(!localStorage.getItem("nextTaskId")){
+        localStorage.setItem("nextTaskId", "0");
+    }
+
+    if(!localStorage.getItem("tasks")){
+        localStorage.setItem("tasks","[]");
+    }
+}
+
+function guardarTarea(){
+    const todoInput = document.getElementById("todoinput");
+    const taskName = todoInput.value
+    if(taskName !== ""){
+        let currentTaskId = parseInt(localStorage.getItem("nextTaskId"))
+        const newTask = new Task(currentTaskId, taskName, false);
+        let tasks = JSON.parse(localStorage.getItem("tasks"));
+        tasks.push(newTask)
+
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+
+        localStorage.setItem("nextTaskId", `${++currentTaskId}`);
+
+        mostrarAlerta();
+
+        todoInput.value = "";
+    }
+}
+
+function mostrarAlerta(){
+    const alerta = document.getElementById("alerta");
+    alerta.show();
+}
--- src/app.js
+++ src/app.js
@@ -1,41 +1,36 @@
-let nameTask1 = "Task 1";
-let nameTask2 = "Task 2";
-let nameTask3 = "Task 3";
-let nameTask4 = "Task 4";
-let nameTask5 = "Task 5";
-let nameTask6 = "Task 6";
-let nameTask7 = "Task 7";
-let nameTask8 = "Task 8";
-let nameTask9 = "Task 9";
-let nameTask10 = "Task 10";
-
-let statusTask1 = true;
-let statusTask2 = false;
-let statusTask3 = true;
-let statusTask4 = true;
-let statusTask5 = true;
-let statusTask6 = true;
-let statusTask7 = true;
-let statusTask8 = true;
-let statusTask9 = true;
-let statusTask10 = true;
+const localStorage = window.localStorage
 
 init();
 function init (){
-    crearTarea(1,nameTask1, statusTask1);
-    crearTarea(2,nameTask2, statusTask2);
-    crearTarea(3,nameTask3, statusTask3);
-    crearTarea(4,nameTask4, statusTask4);
-    crearTarea(5,nameTask5, statusTask5);
-    crearTarea(6,nameTask6, statusTask6);
-    crearTarea(7,nameTask7, statusTask7);
-    crearTarea(8,nameTask8, statusTask8);
-    crearTarea(9,nameTask9, statusTask9);
-    crearTarea(10,nameTask10, statusTask10);
+    
+    
+    if(!localStorage.getItem("nextTaskId")){
+        localStorage.setItem("nextTaskId", "0");
+    }
+
+    if(!localStorage.getItem("tasks")){
+        localStorage.setItem("tasks","[]");
+    }
+    
+    visualizarTareas();
+
 
 
     actualizarSpanTareasCompletadas();
     actualizarSpanTareasPendientes();
+}
+
+function visualizarTareas(){
+    const tareas = localStorage.getItem("tasks")
+    if(tareas){
+        let listObject = JSON.parse(tareas)
+        var Task = null
+        for(let i = 0; i < listObject.length; i++){
+            const task = listObject[i]
+
+            crearTarea(task.id, task.name, task.status);
+        }
+    }
 }
 
 function actualizarSpanTareasCompletadas(){
@@ -77,73 +72,13 @@ function crearTarea (idTask, nameTask, statusTask){
 }
 
 function calcularTaskPendientes (){
-    let contadorTareasPendientes = 0;
-    if(statusTask1 === false){
-        contadorTareasPendientes++;
-    }
-    if(statusTask2 === false){
-        contadorTareasPendientes++;
-    }
-    if(statusTask3 === false){
-        contadorTareasPendientes++;
-    }
-    if(statusTask4 === false){
-        contadorTareasPendientes++;
-    }
-    if(statusTask5 === false){
-        contadorTareasPendientes++;
-    }
-    if(statusTask6 === false){
-        contadorTareasPendientes++;
-    }
-    if(statusTask7 === false){
-        contadorTareasPendientes++;
-    }
-    if(statusTask8 === false){
-        contadorTareasPendientes++;
-    }
-    if(statusTask9 === false){
-        contadorTareasPendientes++;
-    }
-    if(statusTask10 === false){
-        contadorTareasPendientes++;
-    }
-    return contadorTareasPendientes;
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    return tasks.filter(t => t.status === false).length;
 } 
 
 function calcularTaskCompletadas (){
-    let contadorTareasCompletadas = 0;
-    if(statusTask1 === true){
-        contadorTareasCompletadas++;
-    } 
-    if(statusTask2 === true){
-        contadorTareasCompletadas++;
-    }
-    if(statusTask3 === true){
-        contadorTareasCompletadas++;
-    }
-    if(statusTask4 === true){
-        contadorTareasCompletadas++;
-    }
-    if(statusTask5 === true){
-        contadorTareasCompletadas++;
-    }
-    if(statusTask6 === true){
-        contadorTareasCompletadas++;
-    }
-    if(statusTask7 === true){
-        contadorTareasCompletadas++;
-    }
-    if(statusTask8 === true){
-        contadorTareasCompletadas++;
-    }
-    if(statusTask9 === true){
-        contadorTareasCompletadas++;
-    }
-    if(statusTask10 === true){
-        contadorTareasCompletadas++;
-    }
-    return contadorTareasCompletadas;
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    return tasks.filter(t => t.status === true).length;
 }
 
 function onCheckClick(e) {
@@ -163,28 +98,16 @@ function onCheckClick(e) {
     const taskSpan = document.getElementById("span"+ taskId);
     taskSpan.classList.toggle("task-done");
 
-    if (taskId === '1') {
-        statusTask1 = nuevoCheck;
-    } else if (taskId === '2') {
-        statusTask2 = nuevoCheck;
-    } else if (taskId === '3') {
-        statusTask3 = nuevoCheck;
-    } else if (taskId === '4') {
-        statusTask4 = nuevoCheck;
-    } else if (taskId === '5') {
-        statusTask5 = nuevoCheck;
-    } else if (taskId === '6') {
-        statusTask6 = nuevoCheck;
-    } else if (taskId === '7') {
-        statusTask7 = nuevoCheck;
-    } else if (taskId === '8') {
-        statusTask8 = nuevoCheck;
-    } else if (taskId === '9') {
-        statusTask9 = nuevoCheck;
-    } else if (taskId === '10') {
-        statusTask10 = nuevoCheck;
-    } 
 
+    actualizarStatusTarea(parseInt(taskId), nuevoCheck);
     actualizarSpanTareasCompletadas();
     actualizarSpanTareasPendientes();
 }  
+
+function actualizarStatusTarea(id, nuevoCheck){
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    const taskIndex = tasks.findIndex(t => t.id === id);
+    tasks[taskIndex] = {...tasks[taskIndex], status:nuevoCheck};
+
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+}
